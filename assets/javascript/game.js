6b5e419491b103c38370def1b23df1124ed7bbec
Muscle Everest <danza_seppia@yahoo.com>
2018-07-29 15:14:13 -0500

    further JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
     var taco = {
-        health: 100,
-        attack: 25,
-        baseAttack: 25,
-        counterattack: 15
+        health: 120,
+        attack: 22,
+        baseAttack: 22,
+        counterattack: 14
     }
     
     var sloppyJoe = {
-        health: 120,
-        attack: 15,
-        baseAttack: 15,
-        counterattack: 20
+        health: 140,
+        attack: 13,
+        baseAttack: 13,
+        counterattack: 15
     }
     
     var pineapple = {
-        health: 90,
-        attack: 15,
-        baseAttack: 15,
-        counterattack: 25
+        health: 110,
+        attack: 23,
+        baseAttack: 23,
+        counterattack: 18
     }
     
     var pizza = {
-        health: 150,
-        attack: 15,
-        baseAttack: 15,
-        counterattack: 20
+        health: 160,
+        attack: 12,
+        baseAttack: 12,
+        counterattack: 12
     }
     
     var iceCream = {
-        health: 80,
-        attack: 20,
-        baseAttack: 20,
-        counterattack: 20
+        health: 100,
+        attack: 31,
+        baseAttack: 31,
+        counterattack: 15
     }
     
     var userChoices = ["taco", "sloppyJoe", "pineapple", "pizza", "iceCream"];
     var enemyChoices = [];
+    var enemyDefeated = [];
     
     var player;
     var currentEnemy;
@@ -51,15 +52,17 @@ $(document).ready(function() {
             };
             userChoices = [];
             $(".player").append(document.getElementById(player));
-            for (item in enemyChoices) {
-                $("#user-choice").text("Choose your opponent");
-            };
+            $("#user-choice").text("Choose your opponent");
         } 
         //Second click to choose current opponent
         else if (currentEnemy === undefined) {
-            currentEnemy = $(this).attr("data-food");
-            $(".defender").append(document.getElementById(currentEnemy));
-            $("#user-choice").text("");
+            for (item in enemyChoices) {
+                if (enemyChoices[item] === $(this).attr("data-food")) {
+                    currentEnemy = $(this).attr("data-food");
+                    $(".defender").append(document.getElementById(currentEnemy));
+                    $("#user-choice").text("");
+                }
+            }
         };
     });
 
@@ -68,15 +71,27 @@ $(document).ready(function() {
         eval(player).health = eval(player).health - eval(currentEnemy).counterattack;
         eval(currentEnemy).health = eval(currentEnemy).health - eval(player).attack;
         eval(player).attack = eval(player).attack + eval(player).baseAttack;
-        console.log(eval(player));
-        console.log(eval(currentEnemy));
+        $("#tacoHealth").text("Health: " + taco.health);
+        $("#sloppyJoeHealth").text("Health: " + sloppyJoe.health);
+        $("#pizzaHealth").text("Health: " + pizza.health);
+        $("#iceCreamHealth").text("Health: " + iceCream.health);
+        $("#pineappleHealth").text("Health: " + pineapple.health);
         if (eval(player).health <= 0) {
             $("#user-choice").text("You lost!");
         } else if (eval(currentEnemy).health <= 0) {
             $("#user-choice").text("You won the battle!  Choose a new opponent");
             $(".defeated").append(document.getElementById(currentEnemy));
+            for (item in enemyChoices) {
+                if (enemyChoices[item] === currentEnemy) {
+                    enemyDefeated.push(currentEnemy);
+                    enemyChoices.splice(item, 1);
+                }
+            };
             currentEnemy = undefined;
-        }
+            if (enemyChoices.length === 0) {
+                $("#user-choice").text("You are victorious!");
+            }
+        };
     });
 
     //Click restart button to reset conditions
